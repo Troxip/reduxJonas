eb0653ad4595e941b96921a09c179f42eca3ffa0
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-02-23 00:52:19 +0100

    Redux

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+import store from "./store.js";
+import { Provider } from "react-redux";
+
+store.dispatch({ type: "account/deposit", payload: 250 });
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
